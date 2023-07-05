15e4cd9cec01ad88662ea21cefc9ad1c7a37c8bd
Bartosz Pietrzak <75700168+Bartoliinii@users.noreply.github.com>
2023-07-05 22:22:48 +0200

    Setting up the repository

---
+++ src/reviews_retrieval/reviews.py
@@ -0,0 +1,11 @@
+from dataclasses import dataclass
+from datetime import datetime
+from typing import List
+
+
+@dataclass
+class Reviews:
+    stars: int
+    oldest_review_date: datetime
+    reviews: List[str]
+    thumbs_up_count: List[int]
