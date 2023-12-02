eb7ff9137e5ab3c3bc38fb2f85e3c59dde6e1c69
bpietrzak <75700168+bjpietrzak@users.noreply.github.com>
2023-12-02 11:03:39 +0100

    Kinda working

--- src/web_app/script.js
+++ src/web_app/script.js
@@ -17,22 +17,21 @@ document.getElementById('urlInput').addEventListener('keyup', function (event) {
     }
 });
 
+document.getElementById('urlInput').addEventListener('focus', function (event) {
+    hideAppInfo();
+});
+
 function animateInputField() {
     var inputElement = document.querySelector('.futuristic-input');
-    inputElement.style.marginTop = '-5px'; // Adjust the desired distance
-    inputElement.style.transition = 'margin-top 0.5s ease'; // Adding transition for margin-top
+    inputElement.style.marginTop = '-5px';
+    inputElement.style.transition = 'margin-top 0.5s ease';
 }
 
 function displayErrorMessage(message) {
-    // Create a div for the error message
     var errorMessageElement = document.createElement('div');
     errorMessageElement.textContent = message;
     errorMessageElement.className = 'error-message';
-
-    // Append the error message div to the body
     document.body.appendChild(errorMessageElement);
-
-    // Remove the error message after a certain duration (e.g., 5 seconds)
     setTimeout(function () {
         errorMessageElement.remove();
     }, 5000);
@@ -67,37 +66,101 @@ function pasteUrl() {
             .catch((error) => {
             console.error('Request failed:', error);
             });
-
     }
 }
 
+function hideAppInfo() {
+    var appInfoElement = document.getElementById('appInfo');
+    appInfoElement.innerHTML = '';  // Removes all child elements
+    document.querySelector('.inputField').classList.remove('moveUp');
+}
+
 function displayAppInfo(data) {
-    // Dynamically update the 'appInfo' div with the app information
+
     var appInfoElement = document.getElementById('appInfo');
 
-    // Create elements to display title and reviews
+
     var titleElement = document.createElement('h2');
     titleElement.textContent = data.title;
 
     var reviewsElement = document.createElement('p');
     reviewsElement.textContent = `Reviews: ${data.reviews}`;
 
-    // Create an image element for the icon
+
     var iconElement = document.createElement('img');
     iconElement.src = data.icon;
     iconElement.alt = 'App Icon';
+    var median_colour = getMedianColor(data.icon);
+    console.log(median_colour);
 
-    // Append elements to the 'appInfo' element
-    appInfoElement.innerHTML = ''; // Clear previous content
+
+    appInfoElement.innerHTML = '';
     appInfoElement.appendChild(iconElement);
     appInfoElement.appendChild(titleElement);
     appInfoElement.appendChild(reviewsElement);
 
-    // Move the input field to the top
+
     document.querySelector('.inputField').classList.add('moveUp');
 }
 
 document.getElementById('urlInput').addEventListener('focus', function () {
-    // Move the input field back to the center when it's focused
     document.querySelector('.inputField').classList.remove('moveUp');
 });
+
+
+function calculateAverage(values) {
+    var sum = values.reduce(function (a, b) {
+      return a + b;
+    });
+    var avg = sum / values.length;
+    return avg;
+};
+
+function getMedianColor(imageUrl) {
+    var img = new Image();
+    img.crossOrigin = 'anonymous'; // Enable cross-origin resource sharing
+
+    img.onload = function () {
+        var canvas = document.createElement('canvas');
+        canvas.width = img.width;
+        canvas.height = img.height;
+
+        var ctx = canvas.getContext('2d');
+        ctx.drawImage(img, 0, 0, img.width, img.height);
+
+        var imageData = ctx.getImageData(0, 0, img.width, img.height);
+        var data = imageData.data;
+
+        var redValues = [];
+        var greenValues = [];
+        var blueValues = [];
+
+        // Loop through pixel data and store RGB values
+        for (var i = 0; i < data.length; i += 4) {
+        redValues.push(data[i]);
+        greenValues.push(data[i + 1]);
+        blueValues.push(data[i + 2]);
+        }
+
+        // Calculate median RGB values
+        var medianRed = calculateAverage(redValues);
+        var medianGreen = calculateAverage(greenValues);
+        var medianBlue = calculateAverage(blueValues);
+
+        // Display the median color
+        var resultColor = `rgb(${medianRed.toFixed(0)}, ${medianGreen.toFixed(0)}, ${medianBlue.toFixed(0)})`;
+        console.log('Median Color:', resultColor);
+
+        getMedianColor('your_image_url_here', function(resultColor) {
+            setGradientBackground(resultColor);
+        });
+    };
+
+    function setGradientBackground(averageColor) {
+        var body = document.body;
+        body.style.background = `linear-gradient(to bottom, ${averageColor} 20%, rgb(255, 253, 247) 20%)`;
+        body.style.backgroundSize = '100% 100%';
+    }
+
+    img.src = imageUrl;
+};
